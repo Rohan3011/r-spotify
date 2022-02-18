44e3c3c89916b8cb5f40ff3825b7ae8e351a8188
Rohanop <rohankamble3011@gmail.com>
2022-02-18 20:03:21 +0530

    add tags

--- components/right/Genres.js
+++ components/right/Genres.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const genre = ["Classical", "Minimal", "Hip-hop", "Chillout", "Rock", "Pop", "Heavy Metal", "Jazz", "Folk", "Disco"]
+// const genre = ["Classical", "Minimal", "Hip-hop", "Chillout", "Rock", "Pop", "Heavy Metal", "Jazz", "Folk", "Disco"]
 
-const Genres = () => {
+const Genres = ({ spotifyApi }) => {
+
+    const [genre, setGenre] = useState([])
+    const [showGenre, setShowGenre] = useState(false)
+    useEffect(() => {
+        spotifyApi.getCategories({
+        }).then(function (res) {
+            setGenre(res.body.categories.items)
+        }, function (error) {
+            console.log(error)
+        })
+    }, [spotifyApi, showGenre, genre])
+
     return (
         <div className="p-6 flex flex-col justify-center space-y-2" >
             <h2 className="title">
@@ -10,15 +22,18 @@ const Genres = () => {
             </h2>
             <div className=" flex flex-wrap justify-center algin-center gap-1.5 ">
                 {
-                    genre.slice(0, 6).map((value, index) => (
-                        <div className="tags" key={index}>
-                            {value}
+                    genre.slice(0, showGenre ? 20 : 6).map((val) => (
+                        <div className="tags" key={val.id}>
+                            {val.name}
                         </div>
                     ))
                 }
             </div>
-            <div className="tags flex justify-center items-center mx-2" >
-                <span>All Genres</span>
+            <div onClick={() => setShowGenre(!showGenre)}
+                className="tags flex justify-center items-center mx-2" >
+                <span>
+                    {showGenre ? "Hide" : "View all"}
+                </span>
             </div>
         </div >
     );
